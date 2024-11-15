3338c1a156aaf7bfdbc379878bbd0f96da895833
leobuck <leobuck12@gmail.com>
2024-11-15 09:22:23 -0300

    Recebendo um usuário

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsuarioController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult CadastraUsuario()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
