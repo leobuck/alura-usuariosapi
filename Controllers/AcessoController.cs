74dd4650a29787d5b42783991c3c356b217186dc
leobuck <leobuck12@gmail.com>
2024-11-15 14:16:41 -0300

    Criando a política de acesso

---
+++ Controllers/AcessoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApi.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class AcessoController : ControllerBase
+	{
+		[HttpGet]
+		[Authorize(Policy = "IdadeMinima")]
+		public IActionResult Get()
+		{
+			return Ok("Acesso permitido!");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Authorization;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
 using UsuariosApi.Services;
@@ -25,6 +26,12 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
+
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("IdadeMinima", 
+		policy => policy.AddRequirements(new IdadeMinima(18)));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
