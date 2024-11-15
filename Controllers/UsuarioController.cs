aa836105c8dc6fff3bd0df8a74323b467bad12da
leobuck <leobuck12@gmail.com>
2024-11-15 11:00:29 -0300

    Movendo para um serviço

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,35 +1,25 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.Dtos;
-using UsuariosApi.Models;
+using UsuariosApi.Services;
 
 namespace UsuariosApi.Controllers
 {
 	[Route("[controller]")]
 	[ApiController]
 	public class UsuarioController : ControllerBase
-	{
-		private IMapper _mapper;
-		private UserManager<Usuario> _userManager;
+	{		
+		private CadastroService _cadastroService;
 
-		public UsuarioController(IMapper mapper, UserManager<Usuario> userManager)
+		public UsuarioController(CadastroService cadastroService)
 		{
-			_mapper = mapper;
-			_userManager = userManager;
+			_cadastroService = cadastroService;
 		}
 
 		[HttpPost]
-		public async Task<IActionResult> CadastraUsuarioAsync(CreateUsuarioDto dto)
+		public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
 		{
-			Usuario usuario = _mapper.Map<Usuario>(dto);
-
-			IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Password);
-
-			if (resultado.Succeeded)
-				return Ok("Usuário cadastrado!");
-
-			throw new ApplicationException("Falha ao cadastrar usuário.");
+			await _cadastroService.Cadastra(dto);
+			return Ok("Usuário cadastrado!");
 		}
 	}
 }
